42d2bf9d6b9d940bf5828894e68ae3a134eca911
Peter Metero <meteropeter@gmail.com>
2026-01-30 09:58:36 +0300

    initial commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,11 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 
 // route import
 import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js';
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 // example route http://localhost:8000/api/v1/users/register
 
